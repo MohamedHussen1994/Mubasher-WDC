9c787de3c804d06cd612d30291faf607fab0494d
Mohammed Hussen <mohamedhussen1994@gmail.com>
2020-05-04 22:22:31 +0200

    adding all columns from mubasher api

--- js/wdc.js
+++ js/wdc.js
@@ -1,72 +1,111 @@
 (function () {
-    var myConnector = tableau.makeConnector();
-    
-    // This creates the Web Data Connector schema that
-    // describes the information returned by hte WDC.
-    myConnector.getSchema = function (schemaCallback) {
-       var cols = [{
-          id: "Country",
-          dataType: tableau.dataTypeEnum.string
-       }, {
-          id: "Year",
-          dataType: tableau.dataTypeEnum.int
-       }, {
-          id: "GDP",
-          dataType: tableau.dataTypeEnum.float
-       }];
+   var myConnector = tableau.makeConnector();
+   // This creates the Web Data Connector schema that
+   // describes the information returned by hte WDC.
+   myConnector.getSchema = function (schemaCallback) {
+      var cols = [{
+         id: "exchange",
+         dataType: tableau.dataTypeEnum.string
+      }, {
+        id: "name",
+        dataType: tableau.dataTypeEnum.string
+     }, {
+        id: "url",
+        dataType: tableau.dataTypeEnum.string
+     }, {
+        id: "code",
+        dataType: tableau.dataTypeEnum.string
+     }, {
+         id: "value",
+         dataType: tableau.dataTypeEnum.float
+      }, {
+        id: "change",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "changePercentage",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "turnover",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "open",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "high",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "low",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "volume",
+        dataType: tableau.dataTypeEnum.float
+     }, {
+        id: "status",
+        dataType: tableau.dataTypeEnum.string
+     }, {
+        id: "chartFileUrl",
+        dataType: tableau.dataTypeEnum.string
+     }, {
+         id: "updatedAt",
+         dataType: tableau.dataTypeEnum.string
+      }];
+ 
+      var tableSchema = {
+         id: "WDC1",
+         alias: "Mubasher web data connector",
+         columns: cols
+      };
+      schemaCallback([tableSchema]);
+   };
+ 
+   // This function is called when data is required from the
+   // Web Data Connector.
+   myConnector.getData = function (table, doneCallback) {
+     var request;
+     request = $.ajax({
+       url: "https://cors-anywhere.herokuapp.com/" + "https://www.mubasher.info/api/1/stocks/prices",
+       type: "GET",
+       data: {country: 'eg'}
+     });
   
-       var tableSchema = {
-          id: "WDC1",
-          alias: "GDP by Country and Year",
-          columns: cols
-       };
-       schemaCallback([tableSchema]);
-    };
-  
-    // This function is called when data is required from the
-    // Web Data Connector.
-    myConnector.getData = function (table, doneCallback) {
-       tableData = [];
-  
-       // We are manually adding data, but in future tutorials
-       // we will connect to an external data source using the
-       // ajax function.
-       tableData.push(
-          {"Country": "US", "Year": "2002", "GDP": "10.98"},
-          {"Country": "China", "Year": "2002", "GDP": "1.45"},
-          {"Country": "Japan", "Year": "2002", "GDP": "3.98"},
-          {"Country": "Germany", "Year": "2002", "GDP": "2.01"},
-          {"Country": "France", "Year": "2002", "GDP": "1.45"},
-          {"Country": "United Kingdom", "Year": "2002", "GDP": "1.62"},
-          {"Country": "Brazil", "Year": "2002", "GDP": "0.50"},
-          {"Country": "Russian Federation", "Year": "2002", "GDP": "0.35"},
-          {"Country": "Italy", "Year": "2002", "GDP": "1.23"},
-          {"Country": "India", "Year": "2002", "GDP": "0.52"},
-          {"Country": "US", "Year": "2012", "GDP": "16.24"},
-          {"Country": "China", "Year": "2012", "GDP": "8.23"},
-          {"Country": "Japan", "Year": "2012", "GDP": "5.94"},
-          {"Country": "Germany", "Year": "2012", "GDP": "3.43"},
-          {"Country": "France", "Year": "2012", "GDP": "2.61"},
-          {"Country": "United Kingdom", "Year": "2012", "GDP": "2.46"},
-          {"Country": "Brazil", "Year": "2012", "GDP": "2.25"},
-          {"Country": "Russian Federation", "Year": "2012", "GDP": "2.02"},
-          {"Country": "Italy", "Year": "2012", "GDP": "2.01"},
-          {"Country": "India", "Year": "2012", "GDP": "1.86"}
-       );
-  
-       table.appendRows(tableData);
-       doneCallback();
-    };
-  
-    // This is reqired to register the Web Data Connector.
-    tableau.registerConnector(myConnector);
-  
-    // Once the document has loaded we will attached functionality
-    // to the submitButton.
-    $(document).ready(function () {
-       $("#submitButton").click(function () {
-          tableau.connectionName = "Web Data Connector Part 1";
-          tableau.submit();
-       });
-    });
- })();
+     // Callback handler for success
+     request.done(function (response, textStatus, jqXHR){
+        var jsonObject = response;
+        tableData = [];
+        jsonObject.prices.forEach(
+           function(item, index) {
+           tableData.push({"exchange": item.exchange,
+                           "name": item.name,
+                           "url": item.url,
+                           "code": item.code,
+                           "value": item.value,
+                           "change": item.change,
+                           "changePercentage": item.changePercentage,
+                           "turnover": item.turnover,
+                           "open": item.open,
+                           "high": item.high,
+                           "low": item.low,
+                           "volume": item.volume,
+                           "status": item.status,
+                           "chartFileUrl": item.chartFileUrl,
+                           "updatedAt": item.updatedAt
+                          });
+           }
+        );
+        table.appendRows(tableData);
+        doneCallback();
+           });
+     
+   };
+
+   // This is reqired to register the Web Data Connector.
+   tableau.registerConnector(myConnector);
+   // Once the document has loaded we will attached functionality
+   // to the submitButton.
+   $(document).ready(function () {
+      $("#submitButton").click(function () {
+         tableau.connectionName = "Web Data Connector Part 1";
+         tableau.submit();
+      });
+   });
+})();
